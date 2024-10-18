d3f8bee99c47cc716b7f3c9318c08ee7b768df18
RBryan05 <rg0358032023@unab.edu.sv>
2024-10-18 13:46:19 -0600

    Guia9: Filtrar la información

--- Backend/Controllers/PersonaController.cs
+++ Backend/Controllers/PersonaController.cs
@@ -9,5 +9,12 @@ namespace Backend.Controllers
     {
         [HttpGet("all")]
         public List<PersonaDatos> GetPersonaDatos() => Repository.persona;
+
+        [HttpGet("{id}")]
+        public PersonaDatos GetPersonaDatos(int id) => Repository.persona.FirstOrDefault(x => x.ID == id);
+
+        [HttpGet("search/{search}")]
+
+        public List<PersonaDatos> Get(string search) => Repository.persona.Where(x => x.Name.ToUpper().Contains(search.ToUpper())).ToList();
     }
 }
--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+469c91cb024d56460267a9c425bde8eec40ef415")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18aa1a73865a34ea8f420348b117b9339e6c7ea9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
