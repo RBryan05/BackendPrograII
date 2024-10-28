593d28497cb6ebed40f3f43df17ff7ad07df6f72
RBryan05 <rg0358032023@unab.edu.sv>
2024-10-28 10:13:46 -0600

    Guia 17:  Creación de DTO

---
+++ Backend/Controllers/BeerController.cs
@@ -0,0 +1,50 @@
+﻿using Backend.DTOs;
+using Backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeerController : ControllerBase
+    {
+        private StoreContext _storeContext;
+
+        public BeerController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<BeerDto>> Get() =>
+            await _storeContext.Beers.Select(x => new BeerDto
+            {
+                Id = x.BeerId,
+                Al = x.Al,
+                BrandId = x.BrandId,
+                Name = x.Name
+            }).ToListAsync();
+
+        [HttpGet("id")]
+        public async Task<ActionResult<BeerDto>> GetById(int id)
+        {
+            var beer = await _storeContext.Beers.FindAsync(id);
+            if (beer == null)
+            {
+                return NotFound();
+            }
+
+            var beerDto = new BeerDto
+            {
+                Id = beer.BeerId,
+                Al = beer.Al,
+                BrandId = beer.BrandId,
+                Name = beer.Name
+            };
+
+            return Ok(beerDto);
+        }
+    }
+}
--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f8ec351ecf8fefca4e1ecb94afb896c3ab43ef2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+257f37f132a3ab8dbdeb4edb6bf7922d9dc26ed6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
